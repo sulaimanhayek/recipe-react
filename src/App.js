c4ff1c208ab616d2d9ed0f034a312288974b7106
Sulaiman <suleiman_hayek@yahoo.com>
2020-07-24 12:45:38 +0300

    looping through the recipes and show the restults

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import { useState } from 'react';
 
 function App() {
   const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
 
   const APP_ID = "4f80f3a2";
   const APP_KEY = "64333253e96ea258752e13b32b8cd82f	";
   const Url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
   const getData = async () => {
     const result = await Axios.get(Url);
+    setRecipes(result.data.hits);
     console.log(result);
+    setQuery("");
   }
 
   const onSubmit = (e) => {
@@ -30,12 +33,21 @@ function App() {
     <div className="App">
       <h1 onClick={getData}>Sulo Recipes</h1>
     <form className="search-form" onSubmit={onSubmit}>
-      <input type="text" placeholder="Search" autoComplete="off" onChange={onChange}></input>
+      <input type="text" placeholder="Search" autoComplete="off" 
+      onChange={onChange}
+      value={query}></input>
       <input type="submit" value="search"></input>
     </form>
+    <div className="recipes">
+      {recipes !== [] && recipes.map(recipe =>
+        <h2>{recipe.recipe.label}</h2>)}
+    </div>
     </div>
 
   );
 }
 
 export default App;
+
+
+// TODO: get the recipe
