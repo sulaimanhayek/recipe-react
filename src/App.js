9ca04906407586c6295f564b964ce79c162496d2
Sulaiman <suleiman_hayek@yahoo.com>
2020-07-24 14:26:16 +0300

    adding alert

--- src/App.js
+++ src/App.js
@@ -5,36 +5,49 @@ import Axios from 'axios'
 import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import Recipe from './components/Recipe'
+import Alert from "./components/Alert";
 
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
 
   const APP_ID = "4f80f3a2";
   const APP_KEY = "64333253e96ea258752e13b32b8cd82f	";
   const Url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
   const getData = async () => {
-    const result = await Axios.get(Url);
-    setRecipes(result.data.hits);
-    console.log(result);
-    setQuery("");
-  }
+    if (query !== "") {
+      const result = await Axios.get(Url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
     getData();
 
-  }
+  };
 
   const onChange = (e) => { 
     setQuery(e.target.value);
 
-  }
+  };
   return (
     <div className="App">
       <h1 onClick={getData}>Sulo Recipes</h1>
     <form className="search-form" onSubmit={onSubmit}>
-      <input type="text" placeholder="Search" autoComplete="off" 
+        {alert !== "" && <Alert alert={alert} />}
+
+      <input type="text" placeholder="Search, ex: pizza" autoComplete="off" 
       onChange={onChange}
       value={query}></input>
       <input type="submit" value="search"></input>
