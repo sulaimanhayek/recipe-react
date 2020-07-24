f74e4f8d06497b7936b3448ce310812b812b7f2c
Sulaiman <suleiman_hayek@yahoo.com>
2020-07-24 14:00:33 +0300

    finalizing the button and the recipe details

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import RecipeDetails from './RecipeDetails'
-const Recipe = ({recipe}) => {
-    const {label, image, url, ingradients} = recipe.recipe;
+const Recipe = ({ recipe }) => {
+    const [show, setShow] = useState(false);
+    const { label, image, url, ingredients } = recipe.recipe;
     return (
         <div className="recipe">
 
-    <h2>{label}</h2>
-    <img src={image} alt={label}></img>
-    <a href={url} target="_blank" rel="noopener noreferrer">URL</a>
-        <button>ingradients</button>
-        <RecipeDetails></RecipeDetails>
+            <h2>{label}</h2>
+            <img src={image} alt={label}></img>
+            <a href={url} target="_blank" rel="noopener noreferrer">URL</a>
+            <button onClick={() => setShow(!show)}>ingredients</button>
+            {show && <RecipeDetails ingredients={ingredients} />}
         </div>
     )
 }
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const RecipeDetails = () => {
-    return (
-        <div>
-            Recipe Details
-        </div>
-    )
-}
-
-export default RecipeDetails
+const RecipeDetails = ({ ingredients }) => {
+    return ingredients.map(ingredient => {
+        return (
+            <ul key={uuidv4()} className="ingredient-list">
+                <li className="ingredient-text">{ingredient.text}</li>
+                <li className="ingredient-weight">Weight - {ingredient.weight}</li>
+            </ul>
+        );
+    });
+};
+export default RecipeDetails;
